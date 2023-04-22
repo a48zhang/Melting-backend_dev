b404e930f17ab0d60ac81c644bdc31479a69a739
a48zhang <107384053+a48zhang@users.noreply.github.com>
2023-04-22 21:11:21 +0800

    fix: bug

--- service/echo/echo.go
+++ service/echo/echo.go
@@ -2,13 +2,11 @@ package echo
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/websocket"
 	"main/service/ws"
 )
 
 func echo(data ws.WsData, r *websocket.Conn, _ chan int) {
-	fmt.Println("recv: ", data)
 	data.BinData, _ = json.Marshal(data)
 	r.WriteMessage(websocket.TextMessage, data.BinData)
 }
